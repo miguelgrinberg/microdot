680cd9c023352f0ff03d67f1041ea174b7b7385b
Miguel Grinberg <miguel.grinberg@gmail.com>
2023-02-21 15:17:52 +0000

    Async example of static file serving

---
+++ examples/static/static_async.py
@@ -0,0 +1,19 @@
+from microdot_asyncio import Microdot
+from microdot import send_file
+app = Microdot()
+
+
+@app.route('/')
+async def index(request):
+    return send_file('static/index.html')
+
+
+@app.route('/static/<path:path>')
+async def static(request, path):
+    if '..' in path:
+        # directory traversal is not allowed
+        return 'Not found', 404
+    return send_file('static/' + path)
+
+
+app.run(debug=True)
