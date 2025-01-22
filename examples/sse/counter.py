71b356c59c716309620759012cc9f44097d0f45b
Miguel Grinberg <miguel.grinberg@gmail.com>
2025-01-22 23:40:37 +0000

    refactor example for clarity

--- examples/sse/counter.py
+++ examples/sse/counter.py
@@ -1,5 +1,5 @@
 import asyncio
-from microdot import Microdot
+from microdot import Microdot, send_file
 from microdot.sse import with_sse
 
 app = Microdot()
@@ -7,47 +7,22 @@ app = Microdot()
 
 @app.route("/")
 async def main(request):
-    html = """
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <title>Microdot SSE Example</title>
-            <meta charset="UTF-8">
-        </head>
-        <body>
-            <h1>Microdot SSE Example</h1>
-            <script>
-            // Create a new EventSource instance with the endpoint URL
-            const eventSource = new EventSource('/events');
-
-            // Listen for incoming messages
-            eventSource.onmessage = (event) => {
-                console.log('Received message:', event.data);
-            };
-
-            // Handle connection errors
-            eventSource.onerror = (error) => {
-                console.error('EventSource failed:', error);
-            };
-
-            // Optionally handle connection opening
-            eventSource.onopen = () => {
-                console.log('Connection to server opened.');
-            };
-            </script>
-        </body>
-    </html>
-    """
-    return html, 200, {'Content-Type': 'text/html'}
+    return send_file('index.html')
 
 
 @app.route('/events')
 @with_sse
 async def events(request, sse):
-    i = 0
-    while True:
-        await asyncio.sleep(1)
-        i += 1
-        await sse.send({'counter': i})
+    print('Client connected')
+    try:
+        i = 0
+        while True:
+            await asyncio.sleep(1)
+            i += 1
+            await sse.send({'counter': i})
+    except asyncio.CancelledError:
+        pass
+    print('Client disconnected')
 
-app.run(debug=True)
+
+app.run()
