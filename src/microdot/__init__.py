de814acad2d8952cecaaa1fe1bdd29d27c6b88bb
Miguel Grinberg <miguel.grinberg@gmail.com>
2025-12-21 10:50:51 +0000

    Version 2.5.2.dev0

--- src/microdot/__init__.py
+++ src/microdot/__init__.py
@@ -1,4 +1,4 @@
 from microdot.microdot import Microdot, Request, Response, abort, redirect, \
     send_file, URLPattern, AsyncBytesIO, iscoroutine  # noqa: F401
 
-__version__ = '2.5.1'
+__version__ = '2.5.2.dev0'
